186e11548875afaf11811785466b17b5fc21c0a0
Oliver <oliver@axismedtech.com>
2016-03-02 00:07:50 -0800

    stuff

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,20 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'app',
     template: `
-        <h3>Angular 2 Boilerplate</h3>
-        <p>Hello World!</p>
+        <h3 (click)="onSelect()" [class.clicked]="showDetails === true">{{contact.firstName}} {{contact.lastName}}</h3>
+        <input [(ngModel)]="contact.firstName" type="text">
+        <div *ngIf="showDetails === true">
+            Phone Number: {{contact.phone}} <br>
+            E-mail: {{contact.email}}
+        </div>
     `,
+    styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
-    public contact = (firstName: "Jacob", lastName: "Smiley", phone: "510-867-5309",  email: "jacob.smiley@gmail.com");
+    public contact = {firstName: "Jacob", lastName: "Smiley", phone: "510-867-5309",  email: "jacob.smiley@gmail.com"};
+    public showDetails = false;
+
+    onSelect() {
+        this.showDetails = true;
+    }
 }
