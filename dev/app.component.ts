fb103e8d27f5fa44e65d6acc496c951cd2913459
Oliver <oliver@axismedtech.com>
2016-03-01 22:34:37 -0800

    stuff

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
+        <h3>Angular 2 Boilerplate</h3>
         <p>Hello World!</p>
     `,
 })
 export class AppComponent {
-
+    public contact = (firstName: "Jacob", lastName: "Smiley", phone: "510-867-5309",  email: "jacob.smiley@gmail.com");
 }
